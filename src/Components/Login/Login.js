133b4f1e04afae8899a58be163a4424fd5d6a992
Rameez <Rameez-Un-Nabi>
2023-10-05 17:30:48 +0500

    Login and SignUp are updated with new features

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,86 +1,91 @@
-import React, { useState } from 'react';
-import './Login.css';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { API_URL } from '../../config';
 
 const Login = () => {
+  const [password, setPassword] = useState("");
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
 
-  const handleEmailChange = (e) => {
-    const value = e.target.value;
-    setEmail(value);
-    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (!emailPattern.test(value)) {
-      setEmailError('Invalid email format');
-    } else {
-      setEmailError('');
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    if (sessionStorage.getItem("auth-token")) {
+      navigate("/")
     }
-  };
+  }, [navigate]);
 
-  const handlePasswordChange = (e) => {
-    const value = e.target.value;
-    setPassword(value);
-    if (value.length < 6) {
-      setPasswordError('Password must be at least 6 characters');
+  const login = async (e) => {
+    e.preventDefault();
+    const res = await fetch(`${API_URL}/api/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
+
+    const json = await res.json();
+    if (json.authtoken) {
+      sessionStorage.setItem('auth-token', json.authtoken);
+      sessionStorage.setItem('email', email);
+      navigate('/');
+      window.location.reload()
     } else {
-      setPasswordError('');
+      if (json.errors) {
+        for (const error of json.errors) {
+          alert(error.msg);
+        }
+      } else {
+        alert(json.error);
+      }
     }
   };
 
   return (
-    <div className="container">
-      <div className="login-grid">
-        <div className="login-text">
-          <h2>Login</h2>
-        </div>
-        <div className="login-text">
-          Are you a new member? <span><a href="../Sign_Up/Sign_Up.html" style={{ color: "#2190FF" }}>Sign Up Here</a></span>
-        </div>
-        <br />
-        <div className="login-form">
-          <form>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input 
-                type="email" 
-                name="email" 
-                id="email" 
-                className="form-control" 
-                placeholder="Enter your email" 
-                aria-describedby="helpId" 
-                value={email}
-                onChange={handleEmailChange}
-              />
-              {emailError && <small className="text-danger">{emailError}</small>}
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input 
-                type="password" 
-                name="password" 
-                id="password" 
-                className="form-control" 
-                placeholder="Enter your password" 
-                aria-describedby="helpId" 
-                value={password}
-                onChange={handlePasswordChange}
-              />
-              {passwordError && <small className="text-danger">{passwordError}</small>}
-            </div>
-            <div className="btn-group">
-              <button type="submit" className="btn btn-primary mb-2 mr-1 waves-effect waves-light">Login</button>
-              <button type="reset" className="btn btn-danger mb-2 waves-effect waves-light">Reset</button>
-            </div>
-            <br />
-            <div className="login-text">
-              Forgot Password?
-            </div>
-          </form>
+    <div>
+      <div className="container">
+        <div className="login-grid">
+          <div className="login-text">
+            <h2>Login</h2>
+          </div>
+          <div className="login-text">
+            Are you a new member? <span><Link to="/signup" style={{ color: '#2190FF' }}> Sign Up Here</Link></span>
+          </div>
+          <br />
+          <div className="login-form">
+            <form onSubmit={login}>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
+              </div>
+              
+              {/* Added Password Input */}
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  name="password"
+                  id="password"
+                  className="form-control"
+                  placeholder="Enter your password"
+                  aria-describedby="helpId"
+                />
+              </div>
+              
+              <div className="btn-group">
+                <button type="submit" className="btn btn-primary mb-2 mr-1 waves-effect waves-light">Login</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default Login;
